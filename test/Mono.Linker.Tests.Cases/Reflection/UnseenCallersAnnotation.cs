0375fe900df2c6171b3a91c6a8e5454ad4b4f772
Alexander Kyte <alkyte@microsoft.com>
2019-03-29 22:03:57 -0400

    add test

---
+++ test/Mono.Linker.Tests.Cases/Reflection/UnseenCallersAnnotation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.Reflection
+{
+	[SetupLinkerArgument("--annotate-unseen-callers")]
+	public class UnseenCallersAnnotation
+	{
+		static Type attrType;
+
+
+		public static void Main()
+		{
+			var typeA = typeof(A);
+			var method = typeA.GetMethod("Foo", BindingFlags.Public);
+			method.Invoke(null, new object[] { });
+		}
+
+		[Kept]
+		public class A
+		{
+			[Kept]
+			[KeptAttributeAttribute("System.Runtime.CompilerServices.ReflectionBlockedAttribute")]
+			public int Foo()
+			{
+				return 42;
+			}
+		}
+
+	}
+}
